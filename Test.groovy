c5a9b1cae4ecb85118b1d445c138796069128a52
MarusiaGyllene <119393945+MarusiaGyllene@users.noreply.github.com>
2024-01-30 16:26:01 +0300

    Update Test.groovy
    добавлено сравнение размера двух списков
    добавлен цикл if else

--- Test.groovy
+++ Test.groovy
@@ -1,11 +1,15 @@
 def my_list = ["велоспед", "велоспед123", "www.имя@обьясняем.рф", "Поеду к к окиану", "мОСКВА", "texst of valeus"]
 def my_resp = ["велосипед", "велосипед 123", "объясняем", "океану", "Москва", "test"]
 count = my_list.size()
+count_r = my_resp.size()
 
-
-while (count>0) {
-	count = count - 1
-	testRunner.testCase.testSteps["SP"].setPropertyValue("value", my_list[count])	
-	testRunner.testCase.testSteps["SP"].setPropertyValue("resp", my_resp[count])	
-	testRunner.testCase.testSteps['SP_Request'].run(testRunner,context)
-}
+if (count == count_r ) {
+	while (count>0) {
+		count = count - 1
+		testRunner.testCase.testSteps["SP"].setPropertyValue("value", my_list[count])	
+		testRunner.testCase.testSteps["SP"].setPropertyValue("resp", my_resp[count])	
+		testRunner.testCase.testSteps['SP_Request'].run(testRunner,context)
+	}
+} else {
+	log.info ("Списки не равны")
+}	
